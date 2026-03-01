69a2d6c8b3eab1809310621beabba218717b8f81
ArtemNik1tin <artemnikitin24453@gmail.com>
2026-03-01 21:15:13 +0300

    test: reorganization and add a few test

---
+++ 01-fsharp-intro/tests/Fibonacci.Tests/MatrixFibonacciTests.fs
@@ -0,0 +1,14 @@
+﻿namespace FibonacciCore.Tests
+
+module FibonacciTests =
+    open NUnit.Framework
+    open FsUnit
+    open FibonacciCore
+
+    [<Test>]
+    let ``getFibonacci should return 0 for 0`` () =
+        MatrixFib.getFibonacci 0UL |> should equal 0UL
+
+    [<Test>]
+    let ``getFibonacci should return 1 for 1`` () =
+        MatrixFib.getFibonacci 1UL |> should equal 1UL
