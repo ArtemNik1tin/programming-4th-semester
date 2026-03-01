d4315c117cd84df9ad9402446d61336adbe6c1b2
ArtemNik1tin <artemnikitin24453@gmail.com>
2026-03-01 18:54:14 +0300

    feat: Add log fib

---
+++ 01-fsharp-intro/Fibonacci.fs
@@ -0,0 +1,30 @@
+module Fibonacci
+
+open System
+
+let rec raiseXToPowerN (x: seq<seq<uint64>>)  (n: uint64) idMatrix matrixMultiply =
+    match n with
+    | 0UL -> idMatrix
+    | 1UL -> x
+    | _ ->
+        let y = raiseXToPowerN x (n / 2UL) idMatrix matrixMultiply
+        let ySquared = matrixMultiply y y
+        if n % 2UL = 1UL then matrixMultiply x ySquared
+        else ySquared
+ 
+let multiplyMatrices (a: seq<seq<uint64>>) (b: seq<seq<uint64>>): seq<seq<uint64>> =
+    let bTranspose = Seq.transpose b
+    Seq.map (fun row ->
+        Seq.map (fun col ->
+            Seq.zip row col |> Seq.sumBy (fun (n, m) -> n * m)) bTranspose) a
+    
+
+let getIdMatrix (size: int): seq<seq<uint64>> =
+    Seq.init size (fun rowIndex -> Seq.init size (fun colIndex -> if rowIndex = colIndex then 1UL else 0UL))
+
+let getFibonacciNumber (n: uint64): Option<uint64> =
+    if n = 0UL then Some 0UL
+    else if n = 1UL then Some 1UL
+    else
+        let F = raiseXToPowerN (List.toSeq [List.toSeq [1UL; 1UL]; List.toSeq [1UL; 0UL]]) n (getIdMatrix 2) multiplyMatrices
+        Some (Seq.item 1 (Seq.item 0 F))
