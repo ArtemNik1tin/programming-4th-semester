6177bfef4555278dfb784b88a1d08d78ee7c8d47
ArtemNik1tin <artemnikitin24453@gmail.com>
2026-03-12 14:14:16 +0300

    feat: add test and impl

---
+++ 04-types-and-generics/src/PointFree/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace PointFree
+
+module PointFree =
+    let funcPointFree = (*) >> List.map
+    let func x l = List.map (fun y -> y * x) l
---
+++ 04-types-and-generics/tests/PointFree.Tests/PointFreeTests.fs
@@ -0,0 +1,15 @@
+﻿namespace PointFree.Tests
+
+open NUnit.Framework
+open FsCheck.NUnit
+open PointFree
+
+[<TestFixture>]
+type PointFreeTests() =
+
+    [<Property>]
+    member _.``funcPointFree should be equivalent to funcOriginal`` (x: int) (l: int list) =
+        let resultOriginal = PointFree.func x l
+        let resultPointFree = PointFree.funcPointFree x l
+
+        resultOriginal = resultPointFree
