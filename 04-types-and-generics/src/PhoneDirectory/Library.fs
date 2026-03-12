a2b799d0f266977d94996a8abfb26cdb5281d9f1
ArtemNik1tin <artemnikitin24453@gmail.com>
2026-03-12 21:39:32 +0300

    feat: add test and cli

---
+++ 04-types-and-generics/src/PhoneDirectory/Library.fs
@@ -0,0 +1,34 @@
+﻿namespace PhoneDirectory
+
+module PhoneDirectory =
+    type Entry = { Name: string; Phone: string }
+    type Directory = Entry list
+
+    let add name phone (directory: Directory) : Directory =
+        { Name = name; Phone = phone } :: directory
+
+    let findPhones name (directory: Directory) : string list =
+        directory |> List.filter (fun e -> e.Name = name) |> List.map (fun e -> e.Phone)
+
+    let findNames phone (directory: Directory) : string list =
+        directory
+        |> List.filter (fun e -> e.Phone = phone)
+        |> List.map (fun e -> e.Name)
+
+    let serialize (directory: Directory) : string list =
+        directory |> List.map (fun e -> sprintf "%s;%s" e.Name e.Phone)
+
+    let deserialize (lines: string list) : Directory =
+        lines
+        |> List.choose (fun line ->
+            let parts = line.Split(';')
+
+            match parts with
+            | [| name; phone |] when
+                not (System.String.IsNullOrWhiteSpace name)
+                && not (System.String.IsNullOrWhiteSpace phone)
+                ->
+                Some
+                    { Name = name.Trim()
+                      Phone = phone.Trim() }
+            | _ -> None)
---
+++ 04-types-and-generics/tests/PhoneDirectory.Tests/PhoneDirectoryTests.fs
@@ -0,0 +1,47 @@
+﻿namespace PhoneDirectory.Tests
+
+module PhoneDirectoryTests =
+    open NUnit.Framework
+    open FsUnit
+    open PhoneDirectory
+
+    [<Test>]
+    let ``add should increase directory size`` () =
+        let initial = []
+        let updated = PhoneDirectory.add "Ivan" "123" initial
+        updated.Length |> should equal 1
+        updated.[0].Name |> should equal "Ivan"
+
+    [<Test>]
+    let ``findPhones should return multiple numbers for same name`` () =
+        let db = [] |> PhoneDirectory.add "Ivan" "111" |> PhoneDirectory.add "Ivan" "222"
+        let results = PhoneDirectory.findPhones "Ivan" db
+        results |> should contain "111"
+        results |> should contain "222"
+        results.Length |> should equal 2
+
+    [<Test>]
+    let ``findNames should return multiple names for same phone`` () =
+        let db = [] |> PhoneDirectory.add "Ivan" "111" |> PhoneDirectory.add "Oleg" "111"
+        let results = PhoneDirectory.findNames "111" db
+        results |> should contain "Ivan"
+        results |> should contain "Oleg"
+        results.Length |> should equal 2
+
+    [<Test>]
+    let ``deserialize should skip corrupted lines`` () =
+        let input = [ "Ivan;123"; "corrupted line"; ";"; "Petr;456" ]
+        let db = PhoneDirectory.deserialize input
+        db.Length |> should equal 2
+        let names = db |> List.map (fun e -> e.Name)
+        names |> should contain "Ivan"
+        names |> should contain "Petr"
+
+    [<Test>]
+    let ``serialize and deserialize should be consistent`` () =
+        let initial =
+            [ { PhoneDirectory.Name = "Test"
+                PhoneDirectory.Phone = "999" } ]
+
+        let result = initial |> PhoneDirectory.serialize |> PhoneDirectory.deserialize
+        result |> should equal initial
