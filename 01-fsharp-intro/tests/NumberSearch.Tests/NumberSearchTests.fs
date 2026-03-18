a50a5c62213069b3939192bad9b10f9b20358cc3
ArtemNik1tin <artemnikitin24453@gmail.com>
2026-03-18 23:09:03 +0300

    test: add main tests

---
+++ 01-fsharp-intro/tests/NumberSearch.Tests/NumberSearchTests.fs
@@ -0,0 +1,30 @@
+﻿namespace NumberSearch.Tests
+
+module NumberSearchTests =
+    open FsUnit
+    open NUnit.Framework
+    open NumberSearch
+
+    [<TestCase(1, 0)>]
+    [<TestCase(2, 1)>]
+    [<TestCase(3, 2)>]
+    [<TestCase(4, 3)>]
+    [<TestCase(5, 4)>]
+    let ``getFirstOccurrence should return correct index for positive numbers`` (inputNumber, expectedIndex) =
+        let result = NumberSearch.getFirstOccurrence [ 1; 2; 3; 4; 5 ] inputNumber
+
+        match result with
+        | Some(resultIndex) -> resultIndex |> should equal expectedIndex
+        | None -> failwith "result is None"
+
+    [<TestCase(-200, 1)>]
+    let ``getFirstOccurrence should return correct index for negative numbers`` (inputNumber, expectedIndex) =
+        let result = NumberSearch.getFirstOccurrence [ 1; -200; 3; 4; 5 ] inputNumber
+
+        match result with
+        | Some(resultIndex) -> resultIndex |> should equal expectedIndex
+        | None -> failwith "result is None"
+
+    [<Test>]
+    let ``getFirstOccurrence should return correct index for empty list`` () =
+        NumberSearch.getFirstOccurrence [] -10 |> should equal None
