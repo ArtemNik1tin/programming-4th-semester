369b93b72b15fa83b1e885bee1b7fb9192150342
ArtemNik1tin <artemnikitin24453@gmail.com>
2026-03-19 23:07:23 +0300

    feat: add main impl and tetst

---
+++ 01-fsharp-intro/src/ReverseList/Library.fs
@@ -0,0 +1,10 @@
+﻿namespace ReverseList
+
+module ReverseList =
+    let getReverseList inputList =
+        let rec getReverseListRecursive resultList currentList =
+            match currentList with
+            | currentHead :: currentTail -> getReverseListRecursive (currentHead :: resultList) currentTail
+            | [] -> resultList
+
+        getReverseListRecursive [] inputList
