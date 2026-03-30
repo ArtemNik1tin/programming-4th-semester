5ead223fcea7d177c9527b70244a86fb2fdb2c7e
Artem Nikitin <artemnikitin24453@gmail.com>
2026-03-30 14:47:06 +0300

    Update 01-fsharp-intro/src/ReverseList/Library.fs
    Co-authored-by: Alexander Bugaev <144491096+bygu4@users.noreply.github.com>

--- 01-fsharp-intro/src/ReverseList/Library.fs
+++ 01-fsharp-intro/src/ReverseList/Library.fs
@@ -1,10 +1,9 @@
-﻿namespace ReverseList
+module ReverseList
 
-module ReverseList =
-    let getReverseList inputList =
-        let rec getReverseListRecursive resultList currentList =
-            match currentList with
-            | currentHead :: currentTail -> getReverseListRecursive (currentHead :: resultList) currentTail
-            | [] -> resultList
+let getReverseList inputList =
+    let rec getReverseListRecursive resultList currentList =
+        match currentList with
+        | currentHead :: currentTail -> getReverseListRecursive (currentHead :: resultList) currentTail
+        | [] -> resultList
 
-        getReverseListRecursive [] inputList
+    getReverseListRecursive [] inputList
