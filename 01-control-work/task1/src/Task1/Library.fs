deca71a06e0c040d266fc7af46636ee36cb632be
ArtemNik1tin <artemnikitin24453@gmail.com>
2026-04-02 15:57:48 +0300

    doc: add header (fix company)

--- 01-control-work/task1/src/Task1/Library.fs
+++ 01-control-work/task1/src/Task1/Library.fs
@@ -1,4 +1,4 @@
-﻿// <copyright file="Library.fs" company="Task1">
+﻿// <copyright file="Library.fs" company="ArtemNik1tin">
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 // </copyright>
 
