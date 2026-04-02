de5df06f99c5739a62ef694090d12f626572327c
ArtemNik1tin <artemnikitin24453@gmail.com>
2026-04-02 15:48:27 +0300

    feat: impl findMin & tests

---
+++ 01-control-work/task1/src/Task1/Library.fs
@@ -0,0 +1,10 @@
+﻿namespace Task1
+
+module ListUtils =
+    /// <summary>Finds the smallest element in a list.</summary>
+    /// <param name="list">Input list of comparable elements.</param>
+    /// <returns>Some smallest element, or None if the list is empty.</returns>
+    let findMin list =
+        match list with
+        | [] -> None
+        | head :: tail -> Some(List.fold (fun acc x -> if x < acc then x else acc) head tail)
