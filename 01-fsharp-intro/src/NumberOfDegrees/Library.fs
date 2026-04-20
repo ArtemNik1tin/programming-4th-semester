791433a6dc2ca2d293cd502f7e55ac1a89bebaa7
ArtemNik1tin <artemnikitin24453@gmail.com>
2026-04-20 19:09:43 +0300

    fix: remove namespace

--- 01-fsharp-intro/src/NumberOfDegrees/Library.fs
+++ 01-fsharp-intro/src/NumberOfDegrees/Library.fs
@@ -1,11 +1,8 @@
-﻿namespace NumberOfDegrees
+﻿module NumberOfDegrees
 
-module NumberOfDegrees =
-    let getListOfPowersOfTwoFromNToM (n: int) (m: int) : Option<list<int>> =
-        if m < n - 1 then
-            None
-        elif m < 0 || n < 0 then
-            None
-        else
-            let list = List.replicate (m - n + 1) 1
-            Some(List.mapi (fun i x -> x <<< i + n) list)
+let getListOfPowersOfTwoFromNToM n m =
+    match m with
+    | _ when m < 0 -> None
+    | _ ->
+        let list = List.replicate (m - n + 1) 1
+        Some(List.mapi (fun i x -> x <<< i + n) list)
