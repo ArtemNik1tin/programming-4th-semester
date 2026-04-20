720e56d279df7aa294b3cf184c3e456ab29167d2
ArtemNik1tin <artemnikitin24453@gmail.com>
2026-04-20 15:19:32 +0300

    fix: remove namespace

--- 01-fsharp-intro/tests/Fibonacci.Tests/MatrixFibonacciTests.fs
+++ 01-fsharp-intro/tests/Fibonacci.Tests/MatrixFibonacciTests.fs
@@ -1,17 +1,20 @@
-﻿namespace FibonacciCore.Tests
+﻿// <copyright file="MatrixFibonacciTests.fs" company="ArtemNik1tin">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
 
-module FibonacciTests =
-    open NUnit.Framework
-    open FsUnit
-    open FibonacciCore
- 
-    [<TestCase(0UL, 0UL)>]
-    [<TestCase(1UL, 1UL)>]
-    [<TestCase(2UL, 1UL)>]
-    [<TestCase(10UL, 55UL)>]
-    let ``getFibonacci should return correct num for base cases`` (n, expected) =
-        MatrixFib.getFibonacci n |> should equal expected
+module FibonacciTests
 
-    [<Test>]
-        let ``F92 is the largest Fibonacci fitting in uint64``() =
-            MatrixFib.getFibonacci 92UL |> should equal 7540113804746346429UL
+open NUnit.Framework
+open FsUnit
+
+[<TestCase(0UL, 0UL)>]
+[<TestCase(1UL, 1UL)>]
+[<TestCase(2UL, 1UL)>]
+[<TestCase(10UL, 55UL)>]
+let ``getFibonacci should return correct num for base cases`` (n, expected) =
+    MatrixFibonacciUtils.getFibonacciNumber n |> should equal expected
+
+[<Test>]
+let ``F92 is the largest Fibonacci fitting in uint64`` () =
+    MatrixFibonacciUtils.getFibonacciNumber 92UL
+    |> should equal 7540113804746346429UL
