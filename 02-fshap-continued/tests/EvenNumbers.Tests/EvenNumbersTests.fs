5280e7504e8839974936d3021f51f2e8245faef1
ArtemNik1tin <artemnikitin24453@gmail.com>
2026-04-22 16:28:04 +0300

    feat: Add impl + tests

---
+++ 02-fshap-continued/tests/EvenNumbers.Tests/EvenNumbersTests.fs
@@ -0,0 +1,21 @@
+﻿module EvenNumbersTests
+
+open FsUnit
+open NUnit.Framework
+open EvenNumbers
+
+[<FsCheck.NUnit.Property(MaxTest = 100)>]
+let MapImplEqualsFoldImpl (source: list<int>) =
+    calculateEvenNumbersUsingMap source = calculateEvenNumbersUsingFold source
+
+[<FsCheck.NUnit.Property(MaxTest = 100)>]
+let FoldImplEqualsFilterImpl (source: list<int>) =
+    calculateEvenNumbersUsingFold source = calculateEvenNumbersUsingFilter source
+
+[<Test>]
+let ``calculateEvenNumbersUsingMap returns 0 for empty list`` () =
+    calculateEvenNumbersUsingMap [] |> should equal 0
+
+[<Test>]
+let ``calculateEvenNumbersUsingMap returns correct number for base case`` () =
+    calculateEvenNumbersUsingMap [ -1; 2; 5; -4; 3 ] |> should equal 2
