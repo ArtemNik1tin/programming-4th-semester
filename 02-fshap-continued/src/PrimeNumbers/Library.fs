2ef57308f36f8a3c77309edcf184afa3a5f52bc6
ArtemNik1tin <artemnikitin24453@gmail.com>
2026-04-22 10:30:41 +0300

    test: Add tests for getPrimeNumbersSeq

---
+++ 02-fshap-continued/src/PrimeNumbers/Library.fs
@@ -0,0 +1,21 @@
+module PrimeNumbers
+
+let getPrimeNumbersSeq =
+    let isPrime n =
+        let rec next isPrime divider =
+            match isPrime, divider with
+            | false, _ -> false
+            | true, 1 -> true
+            | true, divider when divider = n -> next true (divider - 1)
+            | true, divider -> next (not (n % divider = 0)) (divider - 1)
+
+        next true (int (ceil (float n ** (1.0 / 2.0))))
+
+    let rec infinitePrimeNumbers n =
+        seq {
+            match n with
+            | n when isPrime n -> yield! Seq.append (Seq.singleton n) (infinitePrimeNumbers (n + 1))
+            | _ -> yield! infinitePrimeNumbers (n + 1)
+        }
+
+    infinitePrimeNumbers 2
