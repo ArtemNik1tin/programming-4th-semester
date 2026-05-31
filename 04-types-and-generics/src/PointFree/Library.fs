2f0710583906859a0219e807c271d9c8286793c2
ArtemNik1tin <artemnikitin24453@gmail.com>
2026-05-31 11:40:34 +0300

    fix: Add intermediate functions and test their equivalence

--- 04-types-and-generics/src/PointFree/Library.fs
+++ 04-types-and-generics/src/PointFree/Library.fs
@@ -3,11 +3,9 @@
 /// Multiply each element of a list by a given factor.
 ///
 /// Step-by-step transformation to point-free style:
-///
-///   multiplyEach x l = List.map (fun y -> y * x) l    -- Original
-///   multiplyEach x l = List.map ((*) x) l              -- Operator as function: (fun y -> y * x) = ((*) x)
-///   multiplyEach x   = List.map ((*) x)                -- Reduction: remove l
-///   multiplyEach     = (*) >> List.map                 -- Composition: (f >> g) x = g (f x)
-let multiplyEach = (*) >> List.map
-
 let multiplyEachOriginal x l = List.map (fun y -> y * x) l
+let multiplyEachCommutativity x l = List.map (fun y -> x * y) l
+let multiplyEachOperatorAsFunction x l = List.map ((*) x) l
+let multiplyEachReduction x = List.map ((*) x)
+let multiplyEachComposition = (*) >> List.map
+let multiplyEach = (*) >> List.map
--- 04-types-and-generics/tests/PointFree.Tests/PointFreeTests.fs
+++ 04-types-and-generics/tests/PointFree.Tests/PointFreeTests.fs
@@ -8,8 +8,43 @@ open PointFree
 type PointFreeTests() =
 
     [<Property>]
-    member _.``funcPointFree should be equivalent to funcOriginal`` (x: int) (l: int list) =
-        let resultOriginal = multiplyEachOriginal x l
-        let resultPointFree = multiplyEach x l
+    member _.``multiplyEachOriginal should be equivalent to multiplyEachResult`` (x: int) (l: int list) =
+        let multiplyEachOriginalResult = multiplyEachOriginal x l
+        let multiplyEachResult = multiplyEach x l
 
-        resultOriginal = resultPointFree
+        multiplyEachOriginalResult = multiplyEachResult
+
+    [<Property>]
+    member _.``multiplyEachOriginal should be equivalent to multiplyEachCommutativity`` (x: int) (l: int list) =
+        let multiplyEachOriginalResult = multiplyEachOriginal x l
+        let multiplyEachCommutativityResult = multiplyEachCommutativity x l
+
+        multiplyEachOriginalResult = multiplyEachCommutativityResult
+
+    [<Property>]
+    member _.``multiplyEachCommutativity should be equivalent to multiplyEachOperatorAsFunction`` (x: int) (l: int list) =
+        let multiplyEachCommutativityResult = multiplyEachCommutativity x l
+        let multiplyEachOperatorAsFunctionResult = multiplyEachOperatorAsFunction x l
+
+        multiplyEachCommutativityResult = multiplyEachOperatorAsFunctionResult
+
+    [<Property>]
+    member _.``multiplyEachOperatorAsFunction should be equivalent to multiplyEachReduction`` (x: int) (l: int list) =
+        let multiplyEachOperatorAsFunctionResult = multiplyEachOperatorAsFunction x l
+        let multiplyEachReductionResult = multiplyEachReduction x l
+
+        multiplyEachOperatorAsFunctionResult = multiplyEachReductionResult
+
+    [<Property>]
+    member _.``multiplyEachReduction should be equivalent to multiplyEachComposition`` (x: int) (l: int list) =
+        let multiplyEachReductionResult = multiplyEachReduction x l
+        let multiplyEachCompositionResult = multiplyEachComposition x l
+
+        multiplyEachCompositionResult = multiplyEachReductionResult
+
+    [<Property>]
+    member _.``multiplyEachComposition should be equivalent to multiplyEach`` (x: int) (l: int list) =
+        let multiplyEachCompositionResult = multiplyEachComposition x l
+        let multiplyEachResult = multiplyEach x l
+
+        multiplyEachCompositionResult = multiplyEachResult
